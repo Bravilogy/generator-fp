31cd8c5caa422aa31da3a1a48156e92f1e9d7791
Gega Nizharadze <Bravilogy@users.noreply.github.com>
2017-02-27 12:43:33 +0000

    Update functional.js

--- generators/app/templates/_src/store/functional.js
+++ generators/app/templates/_src/store/functional.js
@@ -1,3 +1,4 @@
+/* global global */
 import R from 'ramda';
 import Task from 'data.task';
 import Maybe from 'data.maybe';
@@ -6,4 +7,4 @@ import Either from 'data.either';
 R.compose(R.map(([k, v]) => global[k] = v), R.toPairs)(R);
 
 export const getMessage = new Task((reject, resolve) =>
-    setTimeout(() => resolve('I Can Has Cheezburger?'), 2000))
+    setTimeout(() => resolve('I Can Has Cheezburger?'), 2000));
